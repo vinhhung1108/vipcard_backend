8b342fb89c8df775ea7aa53de93fb24cb322b68f
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2025-04-21 16:50:31 +0700

    Transfrom date interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { TransformDateInterceptor } from './common/interceptors/transform-date.interceptor';
 
 dotenv.config();
 console.log('JWT_SECRET from .env:', process.env.JWT_SECRET); // Thêm dòng này
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TransformDateInterceptor());
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT ?? 3001;
   await app.listen(port, '0.0.0.0');
