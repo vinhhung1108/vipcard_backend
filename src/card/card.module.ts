83b553e05c15a057e8f4a718e5f1d237a4bfa74c
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2025-04-24 13:31:29 +0700

    import modules

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -3,9 +3,17 @@ import { CardService } from './card.service';
 import { CardController } from './card.controller';
 import { Card } from './entities/card.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PartnerModule } from '@src/partner/partner.module';
+import { ServiceModule } from '@src/service/service.module';
+import { ReferralCodeModule } from '@src/referral-code/referral-code.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Card])],
+  imports: [
+    TypeOrmModule.forFeature([Card]),
+    PartnerModule,
+    ServiceModule,
+    ReferralCodeModule,
+  ],
   providers: [CardService],
   controllers: [CardController],
   exports: [CardService],
