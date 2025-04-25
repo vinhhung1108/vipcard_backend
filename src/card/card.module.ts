4b7e37d3029ff408cf4fa1db06abdd9724df3faa
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2025-04-25 12:27:08 +0700

    Add entities to forFeature

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -3,9 +3,12 @@ import { CardService } from './card.service';
 import { CardController } from './card.controller';
 import { Card } from './entities/card.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Service } from '@src/service/entities/service.entity';
+import { Partner } from '@src/partner/entities/partner.entity';
+import { ReferralCode } from '@src/referral-code/entities/referral-code.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Card])],
+  imports: [TypeOrmModule.forFeature([Card, Service, Partner, ReferralCode])],
   providers: [CardService],
   controllers: [CardController],
   exports: [CardService],
