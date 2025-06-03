b8bc232caeed635d07c0eec18d4ff35c1ac60126
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2025-06-03 08:51:10 +0700

    remove code from Update Card

--- src/card/dto/update-card.dto.ts
+++ src/card/dto/update-card.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateCardDto } from './create-card.dto';
 
-export class UpdateCardDto extends PartialType(CreateCardDto) {}
+// Loại bỏ trường 'code' khỏi CreateCardDto, sau đó áp dụng PartialType
+export class UpdateCardDto extends PartialType(
+  OmitType(CreateCardDto, ['code']),
+) {}
